74d3520cabc3454a9b6faaf73d46d3e4678b6061
gooddha <gooddha@ya.ru>
2017-05-25 06:46:39 +0300

    написал сценарий создания БД, исправил открывающий тег на endforeach в templates/my-lots.php

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE yeticave;
+
+USE yeticave;
+
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(255)
+);
+
+CREATE TABLE lots (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dt_add DATETIME,
+  description TEXT,
+  img CHAR(255),
+  price INT UNSIGNED,
+  dt_end DATETIME,
+  price_step INT UNSIGNED,
+  bits_count INT UNSIGNED,
+  seller INT UNSIGNED,
+  winner INT UNSIGNED,
+  category INT UNSIGNED
+);
+
+CREATE TABLE bits (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	dt_add DATETIME,
+	value INT UNSIGNED,
+	user INT UNSIGNED,
+	lot INT UNSIGNED
+);
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	dt_add DATETIME,
+	email CHAR(255),
+	name CHAR(255),
+	password CHAR(64),
+	avatar CHAR(255),
+	contacts TEXT
+);
+
+CREATE UNIQUE INDEX email ON users(email);
