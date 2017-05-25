05fe88947998504bc223da45c57838a0d508fec9
gooddha <gooddha@ya.ru>
2017-05-25 16:50:44 +0300

    пишу запросы

--- queries.sql
+++ queries.sql
@@ -2,16 +2,17 @@
 SELECT * from categories;
 
 -- получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, количество ставок, название категории;
-
+SELECT title, start_price, img, price, bits_count, category FROM lots WHERE dt_add = CURRENT_DATE ;
 
 -- найти лот по его названию или описанию;
-
+SELECT * FROM lots WHERE title LIKE "%?%" OR description LIKE "%?%" ;
 
 -- добавить новый лот (все данные из формы добавления);
+INSERT INTO lots (`title`, `category`, `description`, `img`, `start_price`, `price_step`, `dt_end`) VALUES (?, ?, ?, ?, ?, ?, ?, );
 
 
 -- обновить название лота по его идентификатору;
-
+UPDATE title FROM lots
 
 -- добавить новую ставку для лота;
 
