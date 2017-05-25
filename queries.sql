6845b0884c27cee20f8320979caafa3e37f53791
gooddha <gooddha@ya.ru>
2017-05-25 23:17:18 +0300

    изменил условие вывода новых лотов WHERE dt_end > CURRENT_DATE

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 SELECT * from categories;
 
 -- получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, количество ставок, название категории;
-SELECT title, start_price, img, price, bits_count, category FROM lots WHERE dt_add = CURRENT_DATE ;
+SELECT title, start_price, img, price, bits_count, category FROM lots WHERE dt_end > CURRENT_DATE ;
 
 -- найти лот по его названию или описанию;
 SELECT * FROM lots WHERE title LIKE "%?%" OR description LIKE "%?%" ;
